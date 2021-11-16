9a087b8726d18b95da35db24e95f123c75622958
Rudragouda Patil <rudragouda.spatil1298@gmail.com>
2021-11-16 17:59:15 +0530

    Hashing passwords with bcrypt

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const md5 = require('md5');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -40,17 +40,21 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)   //it gives a hash function to it
-  });
-  newUser.save(function(err){
-    if (err){
-      console.log(err);
-    }else{
-      res.render("secrets")
-    }
-  });
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if (err){
+        console.log(err);
+      }else{
+        res.render("secrets")
+      }
+    });
+});
+
 });
 
 app.post("/login", function(req, res){
@@ -62,11 +66,14 @@ app.post("/login", function(req, res){
       console.log(err);
     } else {
       if (foundUser){
-        if( foundUser.password === password){
-          res.render("secrets")
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+     if(result === true){
+         res.render("secrets")
+     }
+});
+ }
       }
-    }
+
   });
 });
 
